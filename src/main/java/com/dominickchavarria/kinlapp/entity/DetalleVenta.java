c1259601b3b7e0382184279b31ff2cc915c638ca
dchavarria-2025501 <dchavarria-2025501@kinal.edu.gt>
2026-03-26 04:59:15 -0600

    refactor: Code was added to the SaleDetail Entity

--- src/main/java/com/dominickchavarria/kinlapp/entity/DetalleVenta.java
+++ src/main/java/com/dominickchavarria/kinlapp/entity/DetalleVenta.java
@@ -1,4 +1,90 @@
 package com.dominickchavarria.kinlapp.entity;
 
+import jakarta.persistence.*;
+
+import java.math.BigDecimal;
+
+@Entity
+@Table(name = "detalle_venta")
 public class DetalleVenta {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "codigo_detalle_venta")
+    private Long codigoDetalleVenta;
+    @Column
+    private Long cantidad;
+    @Column(name = "precio_unitario")
+    private BigDecimal precioUnitario;
+    @Column
+    private BigDecimal subtotal;
+
+    //RELACION con Producto
+    @ManyToOne
+    @JoinColumn(name = "Productos_codigo_producto")
+    private Producto producto;
+
+    //RELACION con Venta
+    @ManyToOne
+    @JoinColumn(name = "Ventas_codigo_venta")
+    private Venta venta;
+
+    public DetalleVenta(){
+    }
+
+    public DetalleVenta(Long codigoDetalleVenta, Long cantidad, BigDecimal precioUnitario, BigDecimal subtotal, Producto producto, Venta venta) {
+        this.codigoDetalleVenta = codigoDetalleVenta;
+        this.cantidad = cantidad;
+        this.precioUnitario = precioUnitario;
+        this.subtotal = subtotal;
+        this.producto = producto;
+        this.venta = venta;
+    }
+
+    public Long getCodigoDetalleVenta() {
+        return codigoDetalleVenta;
+    }
+
+    public void setCodigoDetalleVenta(Long codigoDetalleVenta) {
+        this.codigoDetalleVenta = codigoDetalleVenta;
+    }
+
+    public Long getCantidad() {
+        return cantidad;
+    }
+
+    public void setCantidad(Long cantidad) {
+        this.cantidad = cantidad;
+    }
+
+    public BigDecimal getPrecioUnitario() {
+        return precioUnitario;
+    }
+
+    public void setPrecioUnitario(BigDecimal precioUnitario) {
+        this.precioUnitario = precioUnitario;
+    }
+
+    public BigDecimal getSubtotal() {
+        return subtotal;
+    }
+
+    public void setSubtotal(BigDecimal subtotal) {
+        this.subtotal = subtotal;
+    }
+
+    public Producto getProducto() {
+        return producto;
+    }
+
+    public void setProducto(Producto producto) {
+        this.producto = producto;
+    }
+
+    public Venta getVenta() {
+        return venta;
+    }
+
+    public void setVenta(Venta venta) {
+        this.venta = venta;
+    }
 }
