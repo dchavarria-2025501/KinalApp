7a5c88a99e81d09456be7e683beae0d8eaa333f2
dchavarria-2025501 <dchavarria-2025501@kinal.edu.gt>
2026-03-26 05:04:43 -0600

    refactor: Code was added in the DetalleVenta Repository

--- src/main/java/com/dominickchavarria/kinlapp/repository/DetalleVentaRepository.java
+++ src/main/java/com/dominickchavarria/kinlapp/repository/DetalleVentaRepository.java
@@ -1,4 +1,10 @@
 package com.dominickchavarria.kinlapp.repository;
 
-public interface DetalleVentaRepository {
+import com.dominickchavarria.kinlapp.entity.DetalleVenta;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface DetalleVentaRepository extends JpaRepository<DetalleVenta, Long> {
+    List<DetalleVenta> findByVentaCodigoVenta(Long codigoVenta);
 }
