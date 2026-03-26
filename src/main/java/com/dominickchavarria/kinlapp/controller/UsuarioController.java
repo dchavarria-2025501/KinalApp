a5d0d5bd6f8ecf8f3070e92c129f235b90d6885a
dchavarria-2025501 <dchavarria-2025501@kinal.edu.gt>
2026-03-26 00:53:52 -0600

    refactor: Code was added to the User Controller

--- src/main/java/com/dominickchavarria/kinlapp/controller/UsuarioController.java
+++ src/main/java/com/dominickchavarria/kinlapp/controller/UsuarioController.java
@@ -1,4 +1,70 @@
 package com.dominickchavarria.kinlapp.controller;
 
-public class UsuarioController {
+import com.dominickchavarria.kinlapp.entity.Usuario;
+import com.dominickchavarria.kinlapp.service.IUsuarioService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import java.util.List;
+
+@RestController
+@RequestMapping("/usuarios")
+public class UsuarioController{
+    private final IUsuarioService usuarioService;
+
+    public UsuarioController(IUsuarioService usuarioService){
+        this.usuarioService = usuarioService;
+    }
+
+    @GetMapping
+    public ResponseEntity<List<Usuario>> listar(){
+        return ResponseEntity.ok(usuarioService.listarTodos());
+    }
+
+    @GetMapping("/{codigoUsuario}")
+    public ResponseEntity<Usuario> buscarPorId(
+            @PathVariable Long codigoUsuario){
+        return usuarioService
+                .buscarPorId(codigoUsuario)
+                .map(ResponseEntity::ok)
+                .orElse(ResponseEntity.notFound().build());
+    }
+
+    @GetMapping("/activos")
+    public ResponseEntity<List<Usuario>> buscarActivos(){
+        List<Usuario> usuarios = usuarioService.buscarActivos();
+        if (usuarios.isEmpty()){
+            return ResponseEntity.noContent().build();
+        }
+        return ResponseEntity.ok(usuarios);
+    }
+
+    @PostMapping
+    public ResponseEntity<?> guardar(
+            @RequestBody Usuario usuario){
+        try{
+            Usuario nuevo = usuarioService.guardar(usuario);
+            return new ResponseEntity<>(nuevo, HttpStatus.CREATED);
+        } catch (IllegalArgumentException e){
+            return ResponseEntity.badRequest().body(e.getMessage());
+        }
+    }
+
+    @PutMapping("/{codigoUsuario}")
+    public ResponseEntity<?> actualizar(@PathVariable Long codigoUsuario, @RequestBody Usuario usuario){
+        if (!usuarioService.existePorId(codigoUsuario)){
+            return ResponseEntity.notFound().build();
+        }
+        Usuario actualizado = usuarioService.actualizar(codigoUsuario, usuario);
+        return ResponseEntity.ok(actualizado);
+    }
+
+    @DeleteMapping("/{codigoUsuario}")
+    public ResponseEntity<Void> eliminar(@PathVariable Long codigoUsuario){
+        if (!usuarioService.existePorId(codigoUsuario)){
+            return ResponseEntity.notFound().build();
+        }
+        usuarioService.eliminar(codigoUsuario);
+        return ResponseEntity.noContent().build();
+    }
 }
