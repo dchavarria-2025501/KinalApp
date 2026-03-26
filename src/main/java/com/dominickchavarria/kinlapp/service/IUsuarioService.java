4fcdaabc1bb19a578dab76a5a1313122f737b939
dchavarria-2025501 <dchavarria-2025501@kinal.edu.gt>
2026-03-26 00:39:58 -0600

    refactor: Code was added to the Interface in the User Service

--- src/main/java/com/dominickchavarria/kinlapp/service/IUsuarioService.java
+++ src/main/java/com/dominickchavarria/kinlapp/service/IUsuarioService.java
@@ -1,4 +1,22 @@
 package com.dominickchavarria.kinlapp.service;
 
-public interface IUsuarioService {
+import com.dominickchavarria.kinlapp.entity.Usuario;
+import java.util.List;
+import java.util.Optional;
+
+public interface IUsuarioService{
+
+    List<Usuario> listarTodos();
+
+    Usuario guardar(Usuario usuario);
+
+    Optional<Usuario> buscarPorId(Long codigoUsuario);
+
+    List<Usuario> buscarActivos();
+
+    Usuario actualizar(Long codigoUsuario, Usuario usuario);
+
+    void eliminar(Long codigoUsuario);
+
+    boolean existePorId(Long codigoUsuario);
 }
