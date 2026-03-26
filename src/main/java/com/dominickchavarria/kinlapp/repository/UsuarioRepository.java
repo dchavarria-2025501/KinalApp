e94ea5a07bcb79a8986bd77ff759c1713bd6653a
dchavarria-2025501 <dchavarria-2025501@kinal.edu.gt>
2026-03-26 00:34:50 -0600

    refactor: Code was added to the User Repository

--- src/main/java/com/dominickchavarria/kinlapp/repository/UsuarioRepository.java
+++ src/main/java/com/dominickchavarria/kinlapp/repository/UsuarioRepository.java
@@ -1,4 +1,11 @@
 package com.dominickchavarria.kinlapp.repository;
 
-public interface UsuarioRepository {
+import com.dominickchavarria.kinlapp.entity.Usuario;
+import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.List;
+import java.util.Optional;
+
+public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
+    List<Usuario> findByEstado(Long estado);
+    Optional<Usuario> findByUsername(String username);
 }
