561e58e7677585ecf4b03a868368322f655188f6
dchavarria-2025501 <dchavarria-2025501@kinal.edu.gt>
2026-03-26 06:20:33 -0600

    refactor: The Clients code was edited so that it is correct

--- src/main/java/com/dominickchavarria/kinlapp/entity/Cliente.java
+++ src/main/java/com/dominickchavarria/kinlapp/entity/Cliente.java
@@ -11,13 +11,13 @@ public class Cliente {
     @Id
     @Column(name = "dpi_cliente")
     private String DPICliente;
-    @Column(name = "nombre_cliente", nullable = false)
+    @Column(name = "nombre_cliente", nullable = false, unique = true)
     private String nombreCliente;
-    @Column(name = "apellido_cliente", nullable = false)
+    @Column(name = "apellido_cliente", nullable = false, unique = true)
     private String apellidoCliente;
-    @Column(nullable = false)
+    @Column(nullable = false, unique = true)
     private String direccion;
-    @Column(nullable = false)
+    @Column(nullable = false, unique = true)
     private int estado;
 
     public Cliente() {
