b17d5c3dba1cbfe8b1f44fcf9201c313b722c8b8
unknown <dchavarria-2025501@kinal.edu.gt>
2026-03-25 20:45:29 -0600

    refactor: The code was fixed correctly

--- src/main/java/com/dominickchavarria/kinlapp/repository/ProductoRepository.java
+++ src/main/java/com/dominickchavarria/kinlapp/repository/ProductoRepository.java
@@ -5,6 +5,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
 
-public interface ProductoRepository extends JpaRepository<Producto,String> {
+public interface ProductoRepository extends JpaRepository<Producto, Integer> {
     List<Producto> findByEstado(int estado);
+
 }
