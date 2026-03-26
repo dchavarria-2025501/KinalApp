836e75befe0b9cb17495e109af17cc812dd128c9
dchavarria-2025501 <dchavarria-2025501@kinal.edu.gt>
2026-03-26 05:45:31 -0600

    refactor: Code was added to the SaleDetail Controller

--- src/main/java/com/dominickchavarria/kinlapp/controller/DetalleVentaController.java
+++ src/main/java/com/dominickchavarria/kinlapp/controller/DetalleVentaController.java
@@ -1,4 +1,42 @@
 package com.dominickchavarria.kinlapp.controller;
 
+import com.dominickchavarria.kinlapp.entity.DetalleVenta;
+import com.dominickchavarria.kinlapp.service.IDetalleVentaService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/detalle_ventas")
 public class DetalleVentaController {
+    private final IDetalleVentaService detalleVentaService;
+
+    public DetalleVentaController(IDetalleVentaService detalleVentaService){
+        this.detalleVentaService = detalleVentaService;
+    }
+
+    @GetMapping
+    public ResponseEntity<List<DetalleVenta>> listar(){
+        return ResponseEntity.ok(detalleVentaService.listarTodos());
+    }
+
+    @GetMapping("/{codigoDetalleVenta}")
+    public ResponseEntity<DetalleVenta> buscarPorId(@PathVariable Long codigoDetalleVenta){
+        return detalleVentaService
+                .buscarPorId(codigoDetalleVenta)
+                .map(ResponseEntity::ok)
+                .orElse(ResponseEntity.notFound().build());
+    }
+
+    @PostMapping
+    public ResponseEntity<DetalleVenta> guardar(@RequestBody DetalleVenta detalleVenta){
+        return ResponseEntity.ok(detalleVentaService.guardar(detalleVenta));
+    }
+    
+    @DeleteMapping("/{codigoDetalleVenta}")
+    public ResponseEntity<Void> eliminar(@PathVariable Long codigoDetalleVenta){
+        detalleVentaService.eliminar(codigoDetalleVenta);
+        return ResponseEntity.noContent().build();
+    }
 }
