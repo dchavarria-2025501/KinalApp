a02ec91f6e25fb8b1f35d51fbf91984d186ab932
dchavarria-2025501 <dchavarria-2025501@kinal.edu.gt>
2026-03-26 02:21:31 -0600

    refactor: Code was added in the Sales Service Interface

--- src/main/java/com/dominickchavarria/kinlapp/service/IVentaService.java
+++ src/main/java/com/dominickchavarria/kinlapp/service/IVentaService.java
@@ -1,4 +1,22 @@
 package com.dominickchavarria.kinlapp.service;
 
-public interface IVentaService {
+import com.dominickchavarria.kinlapp.entity.Venta;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface IVentaService{
+    List<Venta> listarTodos();
+
+    Venta guardar(Venta venta);
+
+    Optional<Venta> buscarPorId(Long codigoVenta);
+
+    List<Venta> buscarActivos();
+
+    Venta actualizar(Long codigoVenta, Venta venta);
+
+    void eliminar(Long codigoVenta);
+
+    boolean existePorId(Long codigoVenta);
 }
