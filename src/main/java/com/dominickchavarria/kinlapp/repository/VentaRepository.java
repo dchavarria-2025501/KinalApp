6899e3df843ab95d4219259f5e40357c4c7b2a26
dchavarria-2025501 <dchavarria-2025501@kinal.edu.gt>
2026-03-26 02:11:30 -0600

    refactor: Code was added to the Sales Repository

--- src/main/java/com/dominickchavarria/kinlapp/repository/VentaRepository.java
+++ src/main/java/com/dominickchavarria/kinlapp/repository/VentaRepository.java
@@ -1,4 +1,9 @@
 package com.dominickchavarria.kinlapp.repository;
 
-public interface VentaRepository {
+import com.dominickchavarria.kinlapp.entity.Venta;
+import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.List;
+
+public interface VentaRepository extends JpaRepository<Venta, Long> {
+    List<Venta> findByEstado(Long estado);
 }
