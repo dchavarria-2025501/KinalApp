9639abf2ff441b5853b58e811df8d00679399581
dchavarria-2025501 <dchavarria-2025501@kinal.edu.gt>
2026-03-26 05:14:39 -0600

    refactor: Code was added to the SaleDetail Service Interface

--- src/main/java/com/dominickchavarria/kinlapp/service/IDetalleVentaService.java
+++ src/main/java/com/dominickchavarria/kinlapp/service/IDetalleVentaService.java
@@ -1,4 +1,22 @@
 package com.dominickchavarria.kinlapp.service;
 
+import com.dominickchavarria.kinlapp.entity.DetalleVenta;
+
+import java.util.List;
+import java.util.Optional;
+
 public interface IDetalleVentaService {
+    List<DetalleVenta> listarTodos();
+
+    DetalleVenta guardar(DetalleVenta detalleVenta);
+
+    Optional<DetalleVenta> buscarPorId(Long codigoDetalleVenta);
+    
+    List<DetalleVenta> buscarPorVenta(Long codigoVenta);
+
+    DetalleVenta actualizar(Long codigoDetalleVenta, DetalleVenta detalleVenta);
+
+    void eliminar(Long codigoDetalleVenta);
+
+    boolean existePorId(Long codigoDetalleVenta);
 }
