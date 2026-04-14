473ef060968ca67dee62f9977caedf97a1975c2c
dchavarria-2025501 <dchavarria-2025501@kinal.edu.gt>
2026-04-14 16:37:54 -0600

    refactor: The code for the ClientViewController was added.

--- src/main/java/com/dominickchavarria/kinlapp/controller/ClienteViewController.java
+++ src/main/java/com/dominickchavarria/kinlapp/controller/ClienteViewController.java
@@ -1,4 +1,39 @@
 package com.dominickchavarria.kinlapp.controller;
 
+import com.dominickchavarria.kinlapp.entity.Cliente;
+import com.dominickchavarria.kinlapp.service.IClienteService;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/vista/clientes")
 public class ClienteViewController {
+
+    private final IClienteService clienteService;
+
+    public ClienteViewController(IClienteService clienteService) {
+        this.clienteService = clienteService;
+    }
+
+    @GetMapping
+    public String listar(Model model) {
+        model.addAttribute("clientes", clienteService.listarTodos());
+        return "clientes";
+    }
+
+    @GetMapping("/nuevo")
+    public String nuevo(Model model) {
+        model.addAttribute("cliente", new Cliente());
+        return "cliente-form";
+    }
+
+    @PostMapping("/guardar")
+    public String guardar(@ModelAttribute("cliente") Cliente cliente) {
+        clienteService.guardar(cliente);
+        return "redirect:/vista/clientes";
+    }
 }
