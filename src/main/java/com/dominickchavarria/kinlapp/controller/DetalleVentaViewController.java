0ac462ece35041f195931db397e65db6109fca97
dchavarria-2025501 <dchavarria-2025501@kinal.edu.gt>
2026-04-15 14:43:49 -0600

    fix: All HTML files were edited.

--- src/main/java/com/dominickchavarria/kinlapp/controller/DetalleVentaViewController.java
+++ src/main/java/com/dominickchavarria/kinlapp/controller/DetalleVentaViewController.java
@@ -7,7 +7,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
 @Controller
-@RequestMapping("/vista/detalleVentas")
+@RequestMapping("/vista/detalle_ventas")
 public class DetalleVentaViewController {
 
     private final IDetalleVentaService detalleVentaService;
